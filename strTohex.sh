b370642690a9297d6af42480e50d508450d13eb8
williamchangTW <williamchangTW@github.com>
2022-10-29 19:32:06 +0800

    [strTohex] first draft
    update headfer content
    
    Signed-off-by: williamchangTW <williamchangTW@github.com>

---
+++ strTohex.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+################################################
+# Information                                  #
+################################################
+# File name: strTOhex.sh
+# Author: William Chang
+# Email: william80121@hotmail.com.tw
+# example string that reply from IPMI command
+# -> 01 00 00 01 00 00 0a fe 09 0a 10 00 15 11 ab 43
