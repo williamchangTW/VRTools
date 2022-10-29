17d139e9e1820be18c9433e26d455584984f9390
williamchangTW <value0314@hotmail.com>
2022-10-29 21:02:53 +0800

    [strTohex] function complete
    accomplish below features:
    * Seperate the string to several bytes.
    * String concat back to a string for IPMI command use.
    
    Signed-off-by: williamchangTW <value0314@hotmail.com>

--- strTohex.sh
+++ strTohex.sh
@@ -7,3 +7,17 @@
 # Email: william80121@hotmail.com.tw
 # example string that reply from IPMI command
 # -> 01 00 00 01 00 00 0a fe 09 0a 10 00 15 11 ab 43
+IPMITOOL="ipmitool raw"
+IPMIReturn="01 00 00 01 00 00 0a fe 09 0a 10 00 15 43 c3 30"
+
+echo ${#IPMIReturn}
+
+# seperate every character in string 
+for ((i=0; i<=${#IPMIReturn}; i+=3))
+do
+    #echo $i
+    echo "0x${IPMIReturn:i:2}"
+    IPMIResult="$IPMIResult 0x${IPMIReturn:i:2}"
+done
+
+echo "$IPMIResult"
