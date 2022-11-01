dcda929903c6698f8e3db544026ec297243be289
williamchangTW <williamchangTW@github.com>
2022-11-01 15:59:06 +0800

    [pmbus] update the script
    below features is done
    * assemply the I2C command
    * reverse the respense data
    * upload to HOST machine is verified
    * Get reversion and ID are all passed
    
    Signed-off-by: williamchangTW <williamchangTW@github.com>

--- pmbus.sh
+++ pmbus.sh
@@ -11,13 +11,14 @@
 ################################################
 # Command Name | Code
 # IC_DEVICE_ID: 0xAD
+# IC_DEVICE_REV: 0xAE
 # MFR_ID: 0x99
 # MFR_MODEL: 0x9A
 # MFR_DATE: 0x9B
-# IC_DEVICE_REV: 0xAE
 # DMAFIX: 0xC5
 # DMASEQ: 0xC6
 # DMAADDR: 0xC7
+IP=""
 I2CGET="i2cget -f -y"
 BUS="7"
 VR_SLAVE_ADDRESS_1="0x5a"
@@ -25,40 +26,62 @@ VR_SLAVE_ADDRESS_2="0x5b"
 
 cmdGetID()
 {
-    code="0xAD"
-    cmd="$I2CGET $BUS $1 $code"
+    # Default read I2C block
+    code="0xad"
+    cmd="$I2CGET $BUS $1 $code s"
+    echo $cmd
+    reverseLSB "$cmd"
 }
 
 cmdGetRivision()
 {
-    code="0xAE"
-    cmd="$I2CGEt $BUS $1 $code"
+    # Default read I2C block
+    code="0xae"
+    cmd="$I2CGET $BUS $1 $code s"
+    echo $cmd
+    reverseLSB "$cmd"
 }
 
 switchOptions()
 {
-    if [ "$2" == "gi" ]; then 
+    echo $2
+    if [ $2 == "i" ]; then 
 	    echo "Get Device ID"
-        cmdGetID
-    elif [ "$2" == "gr" ]; then
+        cmdGetID "$@"
+    elif [ $2 == "r" ]; then
         echo "Get Device Revision"
-        cmdGetRivision
+        cmdGetRivision "$@"
     else 
-	    echo "Your choise is invalid";
-        Help 
+	    echo "Your choise is invalid"
+        Help
+        exit 1
     fi
+}
+
+reverseLSB()
+{
+    echo "reverseLSB"
+    cmd="whoami"
+    response=`$cmd`
+    echo "$response"
+    for ((i=${#response}-1; i>=0; i--))
+    do
+        reverseResult="$reverseResult${response:i:1}"
+    done
+    echo "$reverseResult"
 }
 
 Help()
 {
     # Display all items
+    echo "-----------------------------"
     echo "The script functions is here."
     echo "-----------------------------"
     echo "Syntax: scriptTemplate [-c|V]"
     echo "Options:"
-    echo "gi    Get device ID. (Usage: {0xXX} g)"
-    echo "gr    Get Device Revision."
-    echo "e     Exit this script."
+    echo "i    Get device ID. (Usage: {0xXX} i)"
+    echo "r    Get Device Revision."
+    echo "u    Unbind Driver. (Usage: {0xXX} u)"
     echo "h     Print this Help."
 }
 
@@ -68,21 +91,17 @@ Help()
 main()
 {
     # check slave address is valid
-    while [ true ];
-    do
-        #read -t 5 -n 1
-        if [ "$1" == VR_SLAVE_ADDRESS_1 ]; then
-            echo "Get device from VR slavce address $VR_SLAVE_ADDRESS_1"
-        elif [ "$1" == VR_SLAVE_ADDRESS_2 ]; then
-            echo "Get device from VR slavce address $VR_SLAVE_ADDRESS_2"
-        elif [ "$1" == "e" ]; then
-            echo "exit..."
-            exit 1
-        else
-	        echo "[ERROR] Cannot find VR Controller!"
-            Help
-        fi
-    done
+    if [ $1 == $VR_SLAVE_ADDRESS_1 ]; then
+        echo "Get device from VR slavce address $VR_SLAVE_ADDRESS_1"
+        switchOptions "$@"
+    elif [ $1 == $VR_SLAVE_ADDRESS_2 ]; then
+        echo "Get device from VR slavce address $VR_SLAVE_ADDRESS_2"
+        switchOptions "$@"
+    else
+        echo "[ERROR] Cannot find VR Controller!"
+        Help
+        exit 1
+    fi
 }
 
 main "$@"
